c624032b4e27ce14a02b14f5ec6bec89f93eb4eb
XiaoMouz <1114096791@qq.com>
2023-12-18 23:52:07 +0800

    ref: use ts

--- api/src/index.ts
+++ api/src/index.ts
@@ -1,16 +1,19 @@
-const app = require('express')();
-const { v4 } = require('uuid');
+import { Request, Response } from 'express';
+import express from 'express';
+import { v4 } from 'uuid';
 
-app.get('/api', (req, res) => {
+const app = express();
+
+app.get('/api', (req: Request, res: Response) => {
     const path = `/api/item/${v4()}`;
     res.setHeader('Content-Type', 'text/html');
     res.setHeader('Cache-Control', 's-max-age=1, stale-while-revalidate');
     res.end(`Hello! Go to item: <a href="${path}">${path}</a>`);
 });
 
-app.get('/api/item/:slug', (req, res) => {
+app.get('/api/item/:slug', (req: Request, res: Response) => {
     const { slug } = req.params;
     res.end(`Item: ${slug}`);
 });
 
-module.exports = app;
+export default app;
